87750a8d1d3d7d54dab582300c4afad78a09ce3e
santiago681 <scano9608@gamil.com>
2026-05-08 16:47:16 -0500

    Initial backend setup

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "practice_management_db"
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("Error connecting to the database:", err);
+    } else {
+        console.log("Connected to the MySQL database.");
+    }
+});
+
+module.exports = connection;
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando 🚀");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
+app.get("/users", (req, res) => {
+    const query = "SELECT * FROM users";
+
+    db.query(query, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: "Error al obtener los usuarios" });
+        } else {
+            res.json(results);
+        }
+
+    });
+});
