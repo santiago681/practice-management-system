c30b401d715bcc8bc4971a4e9d17caa3fdc91490
santiago681 <scano9608@gamil.com>
2026-05-09 00:03:45 -0500

    fix: funcion editar usuarios

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -59,6 +59,13 @@ fetch("http://localhost:3000/api/users", {
                 <td>${user.email}</td>
                 <td>${user.role}</td>
                 <td>
+                    <button onclick="openEditModal(
+                        ${user.id},
+                        '${user.name}',
+                        '${user.role}'
+                    )">
+                        Editar
+                    </button>
                     <button onclick="deleteUser(${user.id})">
                         Eliminar
                     </button>
@@ -122,3 +129,49 @@ async function deleteUser(id) {
     }
 
 }
+
+const modal = document.getElementById("editModal");
+const editName = document.getElementById("editName");
+const editRole = document.getElementById("editRole");
+const saveBtn = document.getElementById("saveBtn");
+
+let selectedUserId = null;
+
+function openEditModal(id, name, role) {
+    selectedUserId = id;
+    editName.value = name;
+    editRole.value = role;
+
+    modal.style.display = "flex";
+}
+
+saveBtn.addEventListener("click", async () => {
+    try {
+        const response = await fetch(   
+            `http://localhost:3000/api/users/${selectedUserId}`,
+            {
+                method: "PUT",
+
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+
+                body: JSON.stringify({
+                    name: editName.value,
+                    role: editRole.value
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        alert(data.message || "Usuario actualizado");
+
+        modal.style.display = "none";
+        location.reload();
+    } catch (error) {
+        console.log(error);
+        alert("Error al actualizar usuario");
+    }
+});
